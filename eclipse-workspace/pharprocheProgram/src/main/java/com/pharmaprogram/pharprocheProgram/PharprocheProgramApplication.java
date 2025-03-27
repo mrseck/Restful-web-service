865c0fb775d5c34e50fe39056f72901c02b60651
Ismael Seck <ismaelseck11@gmail.com>
2025-03-27 22:09:37 +0000

    initial commit

---
+++ eclipse-workspace/pharprocheProgram/src/main/java/com/pharmaprogram/pharprocheProgram/PharprocheProgramApplication.java
@@ -0,0 +1,13 @@
+package com.pharmaprogram.pharprocheProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharprocheProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharprocheProgramApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/pharprocheProgram/src/test/java/com/pharmaprogram/pharprocheProgram/PharprocheProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmaprogram.pharprocheProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharprocheProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
